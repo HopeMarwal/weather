0df5a1019f2382c789824f542b98a9cfcb294474
Nadejda <vremea_denighi@mail.ru>
2023-04-08 16:20:18 +0100

    first commit

---
+++ src/assets/img/icons/weatherIcons.js
@@ -0,0 +1,84 @@
+import Sunny from './01.png';
+import MostlySunny from './02.png'
+import PartlySunny from './03.png'
+import IntermittentClouds from './04.png'
+import HazySunshine from './05.png'
+import MostlyCloudy from './06.png'
+import Cloudy from './07.png'
+import Dreary from './08.png'
+import Fog from './11.png'
+import Showers from './12.png'
+import MostlyCloudyShowers from './13.png'
+import PartlySunnyShowers from './14.png'
+import TStorms from './15.png'
+import MostlyCloudyTStorms from './16.png'
+import PartlySunnyTStorms from './17.png'
+import Rain from './18.png'
+import Flurries from './19.png'
+import MostlyCloudyFlurries from './20.png'
+import PartlySunnyFlurries from './21.png'
+import Snow from './22.png'
+import MostlyCloudySnow from './23.png'
+import Ice from './24.png'
+import Sleet from './25.png'
+import FreezingRain from './26.png'
+import RainSnow from './29.png'
+import Hot from './30.png'
+import Cold from './31.png'
+import Windy from './32.png'
+import Clear from './33.png'
+import MostlyClear from './34.png'
+import PartlyCloudy from './35.png'
+import IntermittentCloudsNight from './36.png'
+import HazyMoonlight from './37.png'
+import MostlyCloudyNight from './38.png'
+import PartlyCloudyShowers from './39.png'
+import MostlyCloudyShowersNight from './40.png'
+import PartlyCloudyTStorms from './41.png'
+import MostlyCloudyTStormsNight from './42.png'
+import MostlyCloudyFlurriesNight from './43.png'
+import MostlyCloudySnowNight from './44.png'
+
+export const weatherIcons = [
+  { id: 1, value: Sunny },
+  { id: 2, value: MostlySunny },
+  { id: 3, value: PartlySunny },
+  { id: 4, value: IntermittentClouds },
+  { id: 5, value: HazySunshine },
+  { id: 6, value: MostlyCloudy },
+  { id: 7, value: Cloudy },
+  { id: 8, value: Dreary },
+  { id: 11, value: Fog },
+  { id: 12, value: Showers },
+  { id: 13, value: MostlyCloudyShowers },
+  { id: 14, value: PartlySunnyShowers },
+  { id: 15, value: TStorms },
+  { id: 16, value: MostlyCloudyTStorms },
+  { id: 17, value: PartlySunnyTStorms },
+  { id: 18, value: Rain },
+  { id: 19, value: Flurries },
+  { id: 20, value: MostlyCloudyFlurries },
+  { id: 21, value: PartlySunnyFlurries },
+  { id: 22, value: Snow },
+  { id: 23, value: MostlyCloudySnow },
+  { id: 24, value: Ice },
+  { id: 25, value: Sleet },
+  { id: 26, value: FreezingRain },
+  { id: 29, value: RainSnow },
+  { id: 30, value: Hot },
+  { id: 31, value: Cold },
+  { id: 32, value: Windy },
+  { id: 33, value: Clear },
+  { id: 34, value: MostlyClear },
+  { id: 35, value: PartlyCloudy },
+  { id: 36, value: IntermittentCloudsNight },
+  { id: 37, value: HazyMoonlight },
+  { id: 38, value: MostlyCloudyNight },
+  { id: 39, value: PartlyCloudyShowers },
+  { id: 40, value: MostlyCloudyShowersNight },
+  { id: 41, value: PartlyCloudyTStorms },
+  { id: 42, value: MostlyCloudyTStormsNight },
+  { id: 43, value: MostlyCloudyFlurriesNight },
+  { id: 44, value: MostlyCloudySnowNight },
+
+]
